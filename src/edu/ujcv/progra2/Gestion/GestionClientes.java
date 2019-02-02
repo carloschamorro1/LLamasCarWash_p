5a03ee0eb254b1b4b754a190be45664654101fdf
Carlos <cmchamorro99@hotmail.com>
2019-02-02 11:12:16 -0600

    Agregando funciones para gestion de clientes y gestion de reportes

--- src/edu/ujcv/progra2/Gestion/GestionClientes.java
+++ src/edu/ujcv/progra2/Gestion/GestionClientes.java
@@ -1,4 +1,63 @@
 package edu.ujcv.progra2.Gestion;
 
+import edu.ujcv.progra2.Fileio.LectorCvsCliente;
+import edu.ujcv.progra2.Fileio.EscritorCvsCliente;
+import edu.ujcv.progra2.Modelos.Cliente;
+import edu.ujcv.progra2.Utilidades.LectorTeclado;
+
+import java.util.ArrayList;
+
 public class GestionClientes {
+    private ArrayList<Cliente> clientes = new ArrayList<>();
+
+    public void obtenerClientes() {
+        clientes = LectorCvsCliente.readCsvFile("Clientes.csv");
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente.getIdentidad() + "\t \t" + cliente.getPrimerNombre() + "\t \t \t \t"
+                    + cliente.getPrimerApellido() + "\t \t \t \t" + cliente.getCodigoCliente());
+        }
+    }
+
+    public ArrayList<Cliente> addClient(){
+        LectorTeclado lt = new LectorTeclado();
+        Cliente datos = new Cliente();
+        datos.setIdentidad(lt.leerString("Por favor ingrese la identidad"));
+        datos.setPrimerNombre(lt.leerString("Por favor ingrese el primer nombre"));
+        datos.setPrimerApellido(lt.leerString("Por favor ingrese el primer apellido"));
+        datos.setCodigoCliente(lt.leerString("Por favor ingrese el codigo del cliennte"));
+        clientes.add(new Cliente(datos.getIdentidad(),datos.getPrimerNombre(),datos.getPrimerApellido(), datos.getCodigoCliente()));
+        EscritorCvsCliente.writeCsvFile("Clientes.csv",clientes,true);
+        return clientes;
+    }
+
+    public void deleteClient() {
+        clientes = LectorCvsCliente.readCsvFile("Clientes.csv");
+        int i = 0;
+        for (Cliente cliente:clientes) {
+            i++;
+            if(clientes.size()== i){
+                clientes.remove(clientes.size()-1);
+                EscritorCvsCliente.writeCsvFileE("Clientes.csv",clientes,false);
+                clientes.add(new Cliente("","","",""));
+            }
+        }
+
+    }
+
+
+    public void getClient(){
+        LectorTeclado lt = new LectorTeclado();
+        clientes = LectorCvsCliente.readCsvFile("Clientes.csv");
+        int num = lt.leerEntero("Escriba el indice del cliente que desea obtener", "Error ha ingresado un numero no valido");
+        int i = 0;
+        for (Cliente clientes : clientes) {
+            i++;
+            if(num == i){
+                System.out.println(clientes.getIdentidad() + "\t \t" + clientes.getPrimerNombre() + "\t \t \t \t"
+                        + clientes.getPrimerApellido() + "\t \t \t \t" + clientes.getCodigoCliente());
+            }
+        }
+        if(clientes.size() < num)
+            System.out.println("No existe registro de clientes en ese indice");
+    }
 }
--- src/edu/ujcv/progra2/Gestion/GestionReportes.java
+++ src/edu/ujcv/progra2/Gestion/GestionReportes.java
@@ -1,4 +1,56 @@
 package edu.ujcv.progra2.Gestion;
 
+import edu.ujcv.progra2.Fileio.LectorCvsReportes;
+import edu.ujcv.progra2.Modelos.Reportes;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class GestionReportes {
+    private ArrayList<Reportes> reportes = new ArrayList<>();
+
+
+    public void imprimirLista(){
+        reportes = LectorCvsReportes.readCsvFile("Reportes.csv");
+        for (Reportes reporte: reportes) {
+            System.out.println(reporte.getNombre() + "\t \t \t" + reporte.getRtn() + "\t \t \t " + reporte.getCantidadProductos() + "\t \t \t" + reporte.getTotal());
+        }
+    }
+
+    public void imprimirReporte(){
+        reportes = LectorCvsReportes.readCsvFile("Reportes.csv");
+        double x;
+        int i = 0;
+        int y = 0;
+        System.out.println("\n \t \t \t \t \t \t  REPORTE \n \t \t \t \t \t \t  ======= \n");
+        System.out.println("\t \t \t \t \t CarWash FUERA JOH");
+        System.out.println("\t \t \t \t \t RTN: 08019878659894");
+        System.out.println("\t \t \t Centro Comercial Portal de Viera");
+        System.out.println("\t \t \t Carretera al Hatillo, Tegucigalpa");
+        System.out.println("\t \t \t \t \t Tel: +504 2248-6974");
+        System.out.println("\t \t \t Correo: mercadito_torres@gmail.com");
+        DateFormat hourdateFormat = new SimpleDateFormat("MMMM/yyyy");
+        DateFormat hourdateFormat2 = new SimpleDateFormat("MMMM");
+        Date date = new Date();
+        System.out.println("\t \t \t \t \t" + hourdateFormat.format(date));
+        System.out.println("\n \t \t \t \t" + reportes.size()+ " Facturas en el mes " +hourdateFormat2.format(date)+"\n");
+        System.out.println("Nombre Cliente \t \t \t \t RTN Cliente \t \t Cantidad Productos Facturados \t \t \t Total Venta");
+        for (Reportes reporte: reportes) {
+            i++;
+            System.out.println(reporte.getNombre() + "\t \t \t" + reporte.getRtn() + "\t \t \t \t \t \t" + reporte.getCantidadProductos() + "\t \t \t \t \t \t \t" + reporte.getTotal());
+            x = Double.parseDouble(reporte.getTotal());
+            y += x;
+            if(i == reportes.size()){
+                System.out.println("");
+                System.out.println("Los ingresos en el mes de " + hourdateFormat2.format(date) +  " han sido de " + "L."+y +".00");
+                System.out.println("Los impuestos ha pagar en el mes de " + hourdateFormat2.format(date) +  " han sido de " + "L."+(y*0.15));
+                System.out.println("\t \t \t \t \t    *****");
+                System.out.println("\t \t \t \t  - Reporte Cerrado -");
+            }
+        }
+
+    }
+
 }
